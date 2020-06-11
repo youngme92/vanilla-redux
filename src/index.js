109a2fbac38a2f699aa1ca079245db7df8a566fc
youngme92 <youngme992@gmail.com>
2020-06-11 16:48:14 +0900

    vanilla ToDo 1.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,21 @@
 import {createStore} from 'redux';
-const Plus = document.getElementById("plus");
-const Minus = document.getElementById("minus");
-const number = document.querySelector('span');
 
-// Plus&Minus define
-const Pluscase = "plus";
-const Minuscase = "minus";
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const countModifier = (count = 0, action) => {
-  switch(action.type){
-    case Pluscase:
-      return count + 1;
-    case Minuscase:
-      return count - 1;
-    default:
-      return count;  
-  }
+const createToDo = (toDo) => {
+  const li = document.createElement("li");
+  li.innerText = toDo;
+  ul.appendChild(li);
 }
 
-const countStore = createStore(countModifier);
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  createToDo(toDo);
+}
 
-const onChange = () => number.innerHTML = countStore.getState();
 
-countStore.subscribe(onChange);
-
-Plus.addEventListener('click', () => countStore.dispatch({ type : Pluscase}));
-Minus.addEventListener('click', () =>countStore.dispatch({ type : Minuscase}));
-
+form.addEventListener("submit", onSubmit)
