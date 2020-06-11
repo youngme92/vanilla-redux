3b357b9430105682a8f72ae28be09957c827e77b
youngme92 <youngme992@gmail.com>
2020-06-11 17:30:04 +0900

    state Mutation

--- src/index.js
+++ src/index.js
@@ -4,17 +4,30 @@ const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const createToDo = (toDo) => {
-  const li = document.createElement("li");
-  li.innerText = toDo;
-  ul.appendChild(li);
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state =[], action) => {
+  console.log(action)
+  switch (action.type){
+    case ADD_TODO :
+      return [...state, {text: action.text}];
+    case DELETE_TODO :
+      return [];
+    default:
+      return state;
+  }
 }
+
+const store = createStore(reducer);
+
+store.subscribe(()=> console.log(store.getState()));
 
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  createToDo(toDo);
+  store.dispatch({type: ADD_TODO, text: toDo});
 }
 
 
