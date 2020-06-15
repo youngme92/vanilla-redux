f98272b52c39b0d409aa48261e584a5feefc91c3
youngme92 <youngme992@gmail.com>
2020-06-15 18:08:55 +0900

    React-redux setup

---
+++ src/routes/Home.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+
+function Home(){
+    const [text, setText] = useState("");
+    
+    const onChange = e => {
+        setText(e.target.value);
+    }
+    const onSubmit = e => {
+        e.preventDefault();
+        setText("");
+    }
+    return (
+        <>
+            <h1>To Do</h1>
+            <form onSubmit={onSubmit}>
+                <input type="text" value={text} onChange={onChange} ></input>
+                <button>Add</button>
+            </form>
+            <ul></ul>
+        </>
+    )
+}
+
+export default Home;
